a4c68b9bb71063060cebf30922105c05f9f2eece
ts.igov <ts.igov@outlook.com>
2022-07-13 18:55:03 +0300

    Updated some visuals

--- src/Byteology.Website/Components/LinkButton.razor.cs
+++ src/Byteology.Website/Components/LinkButton.razor.cs
@@ -4,9 +4,6 @@ using Byteology.Website.Theming;
 
 public partial class LinkButton
 {
-    [CascadingParameter(Name = "Theme")]
-    public Theme Theme { get; set; }
-
     [Parameter(CaptureUnmatchedValues = true)]
     public Dictionary<string, object>? InputAttributes { get; set; }
 
