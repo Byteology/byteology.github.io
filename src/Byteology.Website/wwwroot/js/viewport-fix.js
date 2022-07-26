0b01f33e821136d89073c8d75d3c5b592bed410a
ts.igov <ts.igov@outlook.com>
2022-07-26 18:01:10 +0300

    Another mobile test

--- src/Byteology.Website/wwwroot/js/viewport-fix.js
+++ src/Byteology.Website/wwwroot/js/viewport-fix.js
@@ -4,12 +4,11 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
 
     let vh = window.innerHeight * 0.01;
-    console.log(vh);
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-    window.scrollBy(0, 0);
-    alert(vh);
+    alert(window.scrollY);
+    //window.scrollBy(0, 0);
 });
 
 window.addEventListener('scroll', function (e) {
-    console.log(window.scrollY);
+    //console.log(e);
 });
