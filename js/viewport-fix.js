34fb602d0f79bd2f24121d0ca5823588ae364d29
tsIgov <58138531+tsIgov@users.noreply.github.com>
2022-07-26 16:05:58 +0000

    Deploying to release from  @ 3e102c5a58674ed3b11f94486a63fa8ae214b773 🚀

--- js/viewport-fix.js
+++ js/viewport-fix.js
@@ -5,11 +5,12 @@ window.addEventListener('resize', () => {
 
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-    alert("resize");
-    //window.scrollBy(0, 0);
+    //console.log("resize");
+    window.scrollBy(0, 0);
 });
 
 window.addEventListener('scroll', function (e) {
-    //alert("scroll");
+    //console.log("scroll");
+    //window.scrollBy(0, 1);
     //console.log(this.window.scrollY);
 });
