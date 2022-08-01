dd45fe46194e3044c14af7f9043ad8373fb4ff9c
tsIgov <58138531+tsIgov@users.noreply.github.com>
2022-08-01 16:13:25 +0000

    Deploying to release from  @ 0495545c9bc89c359dc1b134813dfdcabc9cae26 🚀

---
+++ js/visual-viewport.js
@@ -0,0 +1,23 @@
+﻿var dotNetObject;
+
+export function init(dotNetObjectReference) {
+    dotNetObject = dotNetObjectReference;
+    setValues();
+
+    window.visualViewport.addEventListener("resize", setValues);
+    window.visualViewport.addEventListener("scroll", setValues);
+}
+
+export function dispose() {
+    window.visualViewport.removeEventListener("resize", setValues);
+    window.visualViewport.removeEventListener("scroll", setValues);
+}
+
+function setValues() {
+    let top = window.visualViewport.offsetTop;
+    let left = window.visualViewport.offsetLeft;
+    let width = window.visualViewport.width;
+    let height = window.visualViewport.height;
+
+    dotNetObject.invokeMethodAsync("onChanged", top, left, width, height);
+}
