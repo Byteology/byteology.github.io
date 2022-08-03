e575699ed1eac8655adcd7f093302397ec2f23ff
tsIgov <58138531+tsIgov@users.noreply.github.com>
2022-08-03 20:03:49 +0000

    Deploying to release from  @ a58c7ed1484f5713d2f6315b8e2569cc90ff7914 🚀

--- js/full-page-scrolling-layout.js
+++ js/full-page-scrolling-layout.js
@@ -1,5 +1,11 @@
 ﻿export function init() {
+    document.documentElement.classList.add("w-full", "h-full");
+    document.body.classList.add("w-full", "h-full");
+    document.getElementById("app").classList.add("w-full", "h-full");
 }
 
 export function dispose() {
+    document.documentElement.classList.remove("w-full", "h-full");
+    document.body.classList.remove("w-full", "h-full");
+    document.getElementById("app").classList.remove("w-full", "h-full");
 }
