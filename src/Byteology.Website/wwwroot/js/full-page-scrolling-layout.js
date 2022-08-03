27fd25674d1ab8e59eea1f063b9eb2025ee0064b
ts.igov <ts.igov@outlook.com>
2022-08-03 19:22:57 +0300

    Simpler snapping

--- src/Byteology.Website/wwwroot/js/full-page-scrolling-layout.js
+++ src/Byteology.Website/wwwroot/js/full-page-scrolling-layout.js
@@ -1,9 +1,5 @@
 ﻿export function init() {
-    document.body.classList.add("no-scrollbar");
-    document.addEventListener("touchend", (e) => { window.scrollTo(0, 800); }) //
-
 }
 
 export function dispose() {
-    document.body.classList.remove("no-scrollbar");
 }
