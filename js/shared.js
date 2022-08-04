54557fc950673a5ea2141f7aaa344e5e58895bd5
tsIgov <58138531+tsIgov@users.noreply.github.com>
2022-08-04 15:52:05 +0000

    Deploying to release from  @ 7ee2ea2983ef0507fc6d2e88483504f49594aa98 🚀

---
+++ js/shared.js
@@ -0,0 +1,11 @@
+﻿window.preventPitbarHiding = () => {
+    document.documentElement.classList.add("w-full", "h-full", "overflow-hidden");
+    document.body.classList.add("w-full", "h-full", "overflow-hidden", "fixed");
+    document.getElementById("app").classList.add("w-full", "h-full", "overflow-hidden");
+};
+
+window.resetPitbarHiding = () => {
+    document.documentElement.classList.remove("w-full", "h-full", "overflow-hidden");
+    document.body.classList.remove("w-full", "h-full", "overflow-hidden", "fixed");
+    document.getElementById("app").classList.remove("w-full", "h-full", "overflow-hidden");
+};
