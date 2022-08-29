7780108f3abe13025d0bbfecf79fe1db2c5d8c99
tsIgov <58138531+tsIgov@users.noreply.github.com>
2022-08-29 14:52:29 +0000

    Deploying to release from  @ 8ac42f4e3c7aae02dd0d5d12b6c3bf4853d75294 🚀

--- js/full-page-scrolling.js
+++ js/full-page-scrolling.js
@@ -1,104 +1,120 @@
-﻿export function init() {
-    preventSlideIncrement = false;
-    preventSlideDecrement = false;
+﻿class FullPageScrolling {
+    isIncrementingSlide;
+    isDecrementingSlide;
 
-    document.getElementById("page").focus();
-    document.getElementById("page").addEventListener("wheel", onPageWheel, { passive: false });
+    overscrollTimer;
+    preventOverscroll;
 
-    let slides = document.getElementsByClassName("slide");
-    for (var i = 0; i < slides.length; i++)
-        slides[i].addEventListener("wheel", onSlideWheel, { passive: false });
-}
+    changeSlideTimer;
 
-var preventSlideIncrement;
-var preventSlideDecrement;
+    inertiaTimer;
 
-function onPageWheel(e) {
-    let layoutTimer;
-
-    if (e.ctrlKey)
-        return;
-
-    e.preventDefault();
-    e.stopPropagation();
-
-    if (isInertiaWheel(e))
-        return;
-
-    let slides = document.getElementsByClassName("slide");
-    let currentSlideIndex;
-    let minDelta = 50000;
-    for (var i = 0; i < slides.length; i++) {
-        let delta = Math.abs(slides[i].getBoundingClientRect().y);
-        if (delta < minDelta) {
-            currentSlideIndex = i;
-            minDelta = delta;
-        }
+    preventPitbarHiding() {
+        document.documentElement.classList.add("w-full", "h-full", "overflow-hidden");
+        document.body.classList.add("w-full", "h-full", "overflow-hidden");
+        document.getElementById("app").classList.add("w-full", "h-full", "overflow-hidden");
     }
 
-    if (currentSlideIndex === undefined)
-        return;
-
-    if (e.deltaY > 0 && !preventSlideIncrement) {
-        clearTimeout(layoutTimer);
-        preventSlideIncrement = true;
-        preventSlideDecrement = false;
-
-        layoutTimer = setTimeout(() => { preventSlideIncrement = false; }, 500);
-
-        if (currentSlideIndex != slides.length - 1) {
-            slides[currentSlideIndex + 1].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
-        }
+    resetPitbarHiding() {
+        document.documentElement.classList.remove("w-full", "h-full", "overflow-hidden");
+        document.body.classList.remove("w-full", "h-full", "overflow-hidden");
+        document.getElementById("app").classList.remove("w-full", "h-full", "overflow-hidden");
     }
-    else if (e.deltaY < 0 && !preventSlideDecrement) {
 
-        clearTimeout(layoutTimer);
-        preventSlideIncrement = false;
-        preventSlideDecrement = true;
-        layoutTimer = setTimeout(() => { preventSlideDecrement = false; }, 500);
+    init() {
+        this.preventSlideIncrement = false;
+        this.preventSlideDecrement = false;
 
-        if (currentSlideIndex != 0)
-            slides[currentSlideIndex - 1].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        document.getElementById("page").focus();
+        document.getElementById("page").addEventListener("wheel", this.onPageWheel, { passive: false });
+
+        let scrollables = document.getElementsByClassName("scrollable");
+        for (var i = 0; i < scrollables.length; i++)
+            scrollables[i].addEventListener("wheel", this.onScrollableWheel, { passive: false });
     }
-}
 
+    onPageWheel(e) {
+        if (e.ctrlKey)
+            return;
 
-function onSlideWheel(e) {
-    let slideTimer;
-    let preventOverscroll;
-
-    if (e.ctrlKey)
-        return;
-
-    if (preventSlideDecrement || preventSlideIncrement) {
         e.preventDefault();
-        return;
+        e.stopPropagation();
+
+        if (fps.isInertiaWheel(e))
+            return;
+
+        let slides = document.getElementsByClassName("slide");
+        let currentSlideIndex;
+        let minDelta = 50000;
+        for (var i = 0; i < slides.length; i++) {
+            let delta = Math.abs(slides[i].getBoundingClientRect().y);
+            if (delta < minDelta) {
+                currentSlideIndex = i;
+                minDelta = delta;
+            }
+        }
+
+        if (currentSlideIndex === undefined)
+            return;
+
+        if (e.deltaY > 0 && !fps.isIncrementingSlide) {
+            clearTimeout(fps.changeSlideTimer);
+            fps.isIncrementingSlide = true;
+            fps.isDecrementingSlide = false;
+
+            fps.changeSlideTimer = setTimeout(() => { fps.isIncrementingSlide = false; }, 500);
+
+            if (currentSlideIndex != slides.length - 1) {
+                slides[currentSlideIndex + 1].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+            }
+        }
+        else if (e.deltaY < 0 && !fps.isDecrementingSlide) {
+
+            clearTimeout(fps.changeSlideTimer);
+            fps.isIncrementingSlide = false;
+            fps.isDecrementingSlide = true;
+
+            fps.changeSlideTimer = setTimeout(() => { fps.isDecrementingSlide = false; }, 500);
+
+            if (currentSlideIndex != 0)
+                slides[currentSlideIndex - 1].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }
     }
 
-    var slide = e.currentTarget;
-    let atBottom = (slide.scrollHeight - slide.scrollTop - slide.clientHeight) < 1;
-    let atTop = slide.scrollTop < 1;
+    onScrollableWheel(e) {
+        if (e.ctrlKey)
+            return;
 
-    if ((e.deltaY > 0 && !atBottom) || (e.deltaY < 0 && !atTop)) {
-        clearTimeout(slideTimer);
-        preventOverscroll = true;
-        slideTimer = setTimeout(() => { preventOverscroll = false; }, 500);
-        e.stopPropagation();
+        if (fps.isDecrementingSlide || fps.isIncrementingSlide) {
+            e.preventDefault();
+            return;
+        }
+
+        var element = e.currentTarget;
+        let atBottom = (element.scrollHeight - element.scrollTop - element.clientHeight) < 1;
+        let atTop = element.scrollTop < 1;
+
+        if ((e.deltaY > 0 && !atBottom) || (e.deltaY < 0 && !atTop)) {
+            clearTimeout(fps.overscrollTimer);
+            fps.preventOverscroll = true;
+            fps.overscrollTimer = setTimeout(() => { fps.preventOverscroll = false; }, 500);
+            e.stopPropagation();
+        }
+        else if (fps.preventOverscroll)
+            e.stopPropagation();
     }
-    else if (preventOverscroll)
-        e.stopPropagation();
+
+    isInertiaWheel(e) {
+        if (Math.abs(e.deltaY) < 50 && fps.inertiaTimer)
+            return true;
+
+        var result = fps.inertiaTimer != undefined;
+
+        clearTimeout(fps.inertiaTimer);
+        fps.inertiaTimer = setTimeout(() => { fps.inertiaTimer = undefined; }, 100);
+
+        return result;
+    }
 }
 
-function isInertiaWheel(e) {
-    let timer;
-
-    if (Math.abs(e.deltaY) < 50)
-        return true;
-
-    var result = timer != undefined;
-
-    clearTimeout(timer);
-    timer = setTimeout(() => { timer = undefined; }, 100);
-
-    return result;
-}
+var fps = new FullPageScrolling();
