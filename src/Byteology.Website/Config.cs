d1433ed1e28cfe6524d4ad28ca5e0b51f96a8d74
ts.igov <ts.igov@outlook.com>
2022-09-10 03:26:20 +0300

    Surpressed a warning

--- src/Byteology.Website/Config.cs
+++ src/Byteology.Website/Config.cs
@@ -1,6 +1,8 @@
 ﻿namespace Byteology.Website;
 
+#pragma warning disable S1075 // URIs should not be hardcoded
 public static class Config
 {
     public const string GoogleDriveInquiryServiceUrl = "GD_INQUIRY_URL";
 }
+#pragma warning restore S1075 // URIs should not be hardcoded
