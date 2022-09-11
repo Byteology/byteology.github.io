f4751954d51becd4a0ac4ee394fc78a3088d650d
ts.igov <ts.igov@outlook.com>
2022-09-12 02:42:32 +0300

    Added swipe dismissal of the nav menu

---
+++ src/Byteology.Website/Components/Navigation/NavigationMenuSmall.razor.cs
@@ -0,0 +1,28 @@
+﻿namespace Byteology.Website.Components.Navigation;
+
+using Microsoft.JSInterop;
+
+public partial class NavigationMenuSmall
+{
+    private readonly NavigationData _data = new();
+
+    [Inject]
+    private IJSRuntime _jsRuntimeAsync { get; set; } = default!;
+    private IJSInProcessRuntime _jsRuntime => (IJSInProcessRuntime)_jsRuntimeAsync;
+
+    private void onClickOutside()
+    {
+        _jsRuntime.InvokeVoid("collapseHamburder");
+    }
+
+    private double _touchStartX;
+    private void onTouchStart(TouchEventArgs args)
+    {
+        _touchStartX = args.Touches.First().ClientX;
+    }
+    private void onTouchMove(TouchEventArgs args)
+    {
+        if (_touchStartX > args.Touches.First().ClientX)
+            _jsRuntime.InvokeVoid("collapseHamburder");
+    }
+}
