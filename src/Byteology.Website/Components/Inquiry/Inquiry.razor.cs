af7d34bf020d541d0c1d9e33e73cca0eb8994aa2
ts.igov <ts.igov@outlook.com>
2022-09-14 17:35:34 +0300

    Improved the call to action section to be more customizable

--- src/Byteology.Website/Components/Inquiry/Inquiry.razor.cs
+++ src/Byteology.Website/Components/Inquiry/Inquiry.razor.cs
@@ -5,19 +5,23 @@ public partial class Inquiry : StyleableComponent
     private bool? _successfulSubmit;
     private bool _shouldFadeIn = false;
 
-    private readonly Model _model;
+    private readonly ResponseModel _responseModel;
+
+    [Parameter]
+    public string? Title { get; set; }
+
+    [Parameter, EditorRequired]
+    public string? CallToAction { get; set; }
 
     public Inquiry()
     {
-        _model = new Model(
-            Title: "Here's to working together",
-            CallToAction: "Don't hesitate to reach out to us with any request and we will help you the best we can. We usually answer within 2 business days.",
-            Response: new ResponseModel(
+        _responseModel = new ResponseModel(
                 OnSuccessTitle: "Thank you!",
                 OnSuccessBody: "Thank you for reaching out! We'll make sure to help you the best we can. We usually answer within 2 business days.",
                 OnErrorTitle: "Oops!",
                 OnErrorBody: "We are terribly sorry but something went wrong. You can either retry or just write us a plain old email.",
-                OnErrorButtonText: "Continue"));
+                OnErrorButtonText: "Continue")
+        ;
     }
 
     private void onSubmit(ContactForm.SubmissionEventArgs args)
@@ -31,7 +35,7 @@ public partial class Inquiry : StyleableComponent
         string? result = null;
 
         if (_successfulSubmit.HasValue)
-            result = _successfulSubmit.Value ? _model.Response.OnSuccessTitle : _model.Response.OnErrorTitle;
+            result = _successfulSubmit.Value ? _responseModel.OnSuccessTitle : _responseModel.OnErrorTitle;
 
         return result ?? string.Empty;
     }
@@ -41,15 +45,10 @@ public partial class Inquiry : StyleableComponent
         string? result = null;
 
         if (_successfulSubmit.HasValue)
-            result = _successfulSubmit.Value ? _model.Response.OnSuccessBody : _model.Response.OnErrorBody;
+            result = _successfulSubmit.Value ? _responseModel.OnSuccessBody : _responseModel.OnErrorBody;
 
         return result ?? string.Empty;
     }
-
-    private sealed record Model(
-        string Title,
-        string CallToAction,
-        ResponseModel Response);
 
     private sealed record ResponseModel(
         string OnSuccessTitle,
--- src/Byteology.Website/Pages/Contacts.razor.cs
+++ src/Byteology.Website/Pages/Contacts.razor.cs
@@ -1,17 +1,16 @@
 ﻿namespace Byteology.Website.Pages;
 
-public partial class Index : ComponentBase
+public partial class Contacts
 {
     private readonly Model _model;
 
-    public Index()
+    public Contacts()
     {
         _model = new Model(
-            Title: "Byteology - a moment of science",
             CallToActionTitle: "Here's to working together",
             CallToAction: "Don't hesitate to reach out to us with any request and we will help you the best we can. We usually answer within 2 business days."
         );
     }
 
-    private sealed record Model(string Title, string CallToActionTitle, string CallToAction);
+    private sealed record Model(string CallToActionTitle, string CallToAction);
 }
