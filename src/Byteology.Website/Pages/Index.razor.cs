75cb7375b037486d12bff97d30f2a7d8f20722b1
ts.igov <ts.igov@outlook.com>
2022-11-03 19:19:17 +0200

    Added head tags to the index page

--- src/Byteology.Website/Pages/Index.razor.cs
+++ src/Byteology.Website/Pages/Index.razor.cs
@@ -3,15 +3,20 @@
 public partial class Index : ComponentBase
 {
     private readonly Model _model;
+    private readonly Head.Model _head;
 
     public Index()
     {
-        _model = new Model(
+        _head = new Head.Model(
             Title: "Byteology - a moment of science",
+            Description: "By introducing scientific generalization to software engineering, Byteology helps businesses tackle software complexity and grow.",
+            Keywords: new string[] { "scientific generalization", "software development", "research", "proof of concept", "poc", "microservices", "migration to microservices", "event sourcing", "consulting", "training", "interviewing", "interviewing as a service" });
+
+        _model = new Model(
             CallToActionTitle: "Here's to working together",
             CallToAction: "Don't hesitate to reach out to us with any request and we will help you the best we can. We usually answer within 2 business days."
         );
     }
 
-    private sealed record Model(string Title, string CallToActionTitle, string CallToAction);
+    private sealed record Model(string CallToActionTitle, string CallToAction);
 }
