2b596a3331ebacf09aebba951aa8ec16bc4da2ff
ts.igov <ts.igov@outlook.com>
2022-11-04 17:52:55 +0200

    Added headers to the existing articles

--- src/Byteology.Website/Components/Head.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Byteology.Website.Components;
-
-public partial class Head : ComponentBase
-{
-    [Parameter, EditorRequired]
-    public Model Data { get; set; } = null!;
-
-    public record Model(string Title, string Description, string[] Keywords);
-}
