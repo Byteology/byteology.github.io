ab1aef298e14cdd25cf633b553b3ca1ea8cf3922
ts.igov <ts.igov@outlook.com>
2022-11-04 17:56:23 +0200

    Fixed a faulty commit

--- src/Byteology.Website/Pages/Index.razor.cs
+++ src/Byteology.Website/Pages/Index.razor.cs
@@ -3,14 +3,15 @@
 public partial class Index : ComponentBase
 {
     private readonly Model _model;
-    private readonly Head.Model _head;
+    private readonly string _title;
+    private readonly string _description;
+    private readonly string[] _keywords;
 
     public Index()
     {
-        _head = new Head.Model(
-            Title: "Byteology - a moment of science",
-            Description: "By introducing scientific generalization to software engineering, Byteology helps businesses tackle software complexity and grow.",
-            Keywords: new string[] { "scientific generalization", "software development", "research", "proof of concept", "poc", "microservices", "migration to microservices", "event sourcing", "consulting", "training", "interviewing", "interviewing as a service" });
+        _title = "Byteology - a moment of science";
+        _description = "By introducing scientific generalization to software engineering, Byteology helps businesses tackle software complexity and grow.";
+        _keywords = new string[] { "scientific generalization", "software development", "research", "proof of concept", "poc", "microservices", "migration to microservices", "event sourcing", "consulting", "training", "interviewing", "interviewing as a service" };
 
         _model = new Model(
             CallToActionTitle: "Here's to working together",
