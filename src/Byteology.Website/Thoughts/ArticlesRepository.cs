40a2d5e9513b50c574dc215a606e9d2372d1b631
ts.Igov <ts.igov@outlook.com>
2023-06-26 16:28:43 +0300

    Added legacy handlers and improved the articles page metadata to include the subtitle

--- src/Byteology.Website/Thoughts/ArticlesRepository.cs
+++ src/Byteology.Website/Thoughts/ArticlesRepository.cs
@@ -27,28 +27,38 @@ public class ArticlesRepository
 		foreach (ArticleMetadata article in articleList.Reverse())
 		{
 			LinkedListNode<ArticleMetadata> node = _data.AddLast(article);
-			bool success = _dict.TryAdd(article.Handle, node);
+			bool success = _dict.TryAdd(article.Handle.ToLower(), node);
 			if (!success)
 				_data.RemoveLast();
+			addLegacyHandles(node);
 		}
+	}
+
+	private void addLegacyHandles(LinkedListNode<ArticleMetadata> node)
+	{
+		if (node.Value.Handle.ToLower() == "recruitment-guide-part-1")
+			_dict.Add("interviewing", node);
 	}
 
 	public IEnumerable<ArticleMetadata> GetAll() => _data.Reverse();
 
 	public ArticleMetadata? Get(string handle)
 	{
+		handle = handle.ToLower();
 		_dict.TryGetValue(handle, out LinkedListNode<ArticleMetadata>? result);
 		return result?.Value;
 	}
 
 	public ArticleMetadata? GetNext(string handle)
 	{
+		handle = handle.ToLower();
 		_dict.TryGetValue(handle, out LinkedListNode<ArticleMetadata>? node);
 		return node?.Next?.Value;
 	}
 
 	public ArticleMetadata? GetPrevious(string handle)
 	{
+		handle = handle.ToLower();
 		_dict.TryGetValue(handle, out LinkedListNode<ArticleMetadata>? node);
 		return node?.Previous?.Value;
 	}
